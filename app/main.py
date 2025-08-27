71a0db94252b4054eca6656e2f2f08be13735b5c
yasuda <samonkntd@gmail.com>
2025-08-27 11:00:53 +0900

    fix: 認証機能-ルーティングの修正

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.ext.asyncio import AsyncSession
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 import uvicorn
 
-from app.db.session import AsyncSessionLocal
+from app.core.limiter import limiter  # 新しいファイルからインポート
 from app.api.v1.endpoints import (
     auths,
     staffs,
     offices,
-    # service_recipient, # コメントアウトされたままのものは一旦そのままにします
-    # support_plan,
-    # dashboard,
 )
 
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # CORSミドルウェアの設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],  # フロントエンドのオリジンと、一旦すべてのオリジンを許可
+    allow_origins=["http://localhost:3000", "https://keikakun-front.vercel.app/"],  # フロントエンドのオリジンと、一旦すべてのオリジンを許可
     allow_credentials=True,
     allow_methods=["*"],  # すべてのメソッドを許可
     allow_headers=["*"],  # すべてのヘッダーを許可
@@ -31,18 +31,9 @@ async def read_root():
 
 
 # APIルーターの登録
-app.include_router(auths.router, prefix="/api/v1/auths", tags=["auths"])
-app.include_router(staffs.router, prefix="/api/v1/staffs", tags=["staffs"])
+app.include_router(auths.router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(staffs.router, prefix="/api/v1/staff", tags=["staff"])
 app.include_router(offices.router, prefix="/api/v1/offices", tags=["offices"])
-# app.include_router(
-#     service_recipient.router,
-#     prefix="/api/v1/service-recipient",
-#     tags=["service-recipient"],
-# )
-# app.include_router(
-#     support_plan.router, prefix="/api/v1/support-plan", tags=["support-plan"]
-# )
-# app.include_router(dashboard.router, prefix="/api/v1/dashboard", tags=["dashboard"])
 
 
 if __name__ == "__main__":
