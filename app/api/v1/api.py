c400886756dfc38340aea440605b772d17d288c4
yasuda <samonkntd@gmail.com>
2025-09-16 15:01:17 +0900

    feat: api_router ダッシュボード

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auths, mfa, offices, staffs, office_staff
+from app.api.v1.endpoints import auths, mfa, offices, staffs, office_staff, dashboard
 
 api_router = APIRouter()
 
@@ -10,3 +10,4 @@ api_router.include_router(staffs.router, prefix="/staffs", tags=["staffs"])
 api_router.include_router(offices.router, prefix="/offices", tags=["offices"])
 api_router.include_router(office_staff.router, prefix="/staff", tags=["staff-office"])
 api_router.include_router(mfa.router, prefix="/auth", tags=["mfa"])
+api_router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
